c64f2701f827622188614dac8b1b1b9a3f71a355
euranium <mountrainier13@gmail.com>
2015-03-13 20:15:29 -0700

    added passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('jsonwebtoken');
+var passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
@@ -12,6 +13,17 @@ router.get('/signUp', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
+	var pass0, pass1, user, email;
+	pass0 = req.body.password0;
+	pass1 = req.body.password1;
+	user = req.body.name;
+	email = req.body.email;
+	if ((pass0 === '') || (pass1 === '') || (pass0 !== pass1)) {
+		res.render('signUp', {});
+	}
+	if ((user === '') || (email === '')) {
+		res.render('signUp', {});
+	}
 	res.render('index', {});
 });
 
