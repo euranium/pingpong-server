1861a55f9277a7f9648f541a2e3efa1555b32b6b
euranium <mountrainier13@gmail.com>
2015-03-20 17:37:22 -0700

    login stuff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // set up ====================================================
-
+var port = 8000;
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
